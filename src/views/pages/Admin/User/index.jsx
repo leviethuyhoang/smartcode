1775af198966d38aab6f1906834ac47d4b31d0cf
Hoang Le <levhuyhoang1@dtu.edu.vn>
2021-10-06 11:59:49 +0700

    Destruct File

--- src/views/pages/Admin/User/index.jsx
+++ src/views/pages/Admin/User/index.jsx
@@ -1,9 +1,15 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { useRouteMatch } from "react-router";
 
 
 const AllUsers = (props) => {
+
+    const match = useRouteMatch();
+    console.log("match user : ", match.url);
     return (
         <Fragment>
+            <Link to = {`${match.url}/add`}> Add User </Link>
             <h1>ALL User</h1>
         </Fragment>
     )
