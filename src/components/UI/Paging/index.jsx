4bf425960bafeb963d4a91635b2794db85b6af24
= <leviethuyhoang262@gmail.com>
2022-02-13 16:08:40 +0700

    asdasd

--- src/components/UI/Paging/index.jsx
+++ src/components/UI/Paging/index.jsx
@@ -105,9 +105,7 @@ const Paging = (props) => {
                     <li>
                     <div className="pagination__link" onClick={prevOffsetPage} > <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-left w-4 h-4"><polyline points="15 18 9 12 15 6" /></svg> </div>
                     </li>
-                    {
-                        renderPageNumber()
-                    }
+                    { renderPageNumber() }
                     <li>
                     <div className="pagination__link" onClick={nextOffsetPage}> <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-right w-4 h-4"><polyline points="9 18 15 12 9 6" /></svg> </div>
                     </li>
